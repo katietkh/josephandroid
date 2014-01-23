bcf1a35c8e1fcd73bf4ffb31eac0079ff75b1341
Katie Hrycak <khrycak@gmail.com>
2014-01-23 12:15:49 -0500

    fresh start

--- CrystalBall/gen/com/example/crystalball/R.java
+++ CrystalBall/gen/com/example/crystalball/R.java
@@ -50,12 +50,7 @@ public final class R {
         public static final int whatever=0x7f02001a;
     }
     public static final class id {
-        public static final int View2=0x7f090004;
-        public static final int action_settings=0x7f090005;
-        public static final int button1=0x7f090001;
-        public static final int imageView1=0x7f090000;
-        public static final int textView1=0x7f090003;
-        public static final int view1=0x7f090002;
+        public static final int action_settings=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- CrystalBall/src/com/example/crystalball/MainActivity.java
+++ CrystalBall/src/com/example/crystalball/MainActivity.java
@@ -1,117 +1,23 @@
 package com.example.crystalball;
 
-import android.app.Activity;
-import android.graphics.drawable.AnimationDrawable;
-import android.hardware.Sensor;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
-	public static final String TAG = MainActivity.class.getSimpleName();
-	
-	private CrystalBall mCrystalBall = new CrystalBall();
-	private TextView mAnswerLabel;
-	private Button mGetAnswerButton;
-	private ImageView mCrystalBallImage;
-	
-	
-	protected void onCreate1(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		// Assigning the views from the layout file
-		mAnswerLabel = (TextView) findViewById(R.id.textView1);
-		mGetAnswerButton = (Button) findViewById(R .id.button1);
-		mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
-		
-		mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View arg0) {
-				String answer = mCrystalBall.getAnAnswer();
-				
-				//Update the label with our answer
-				mAnswerLabel.setText(answer);
-				
-				//Animate the ball and response
-				animateCrystalBall();
-				animateAnswer();
-				playSound();
-			}
-		});
-		
-		//Toast.makeText(this, "Yay! Our Activity was created", Toast.LENGTH_LONG).show();
-		
-		Toast welcomeToast=Toast.makeText(this, "Look at me up here!", Toast.LENGTH_LONG);
-		welcomeToast.setGravity(Gravity.TOP, 0, 0);
-		welcomeToast.show();
-		
-		Log.d(TAG, "We're debugging woot");
-		
-	}
 
-	private void animateCrystalBall() {
-		mCrystalBallImage.setImageResource(R.drawable.ball_animation);
-		AnimationDrawable ballAnimation = (AnimationDrawable) mCrystalBallImage.getDrawable();
-		if (ballAnimation.isRunning()) {
-			ballAnimation.stop();
-		}
-		ballAnimation.start();
-	}
-	//Animation object 
-	private void animateAnswer() {
-		AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
-		fadeInAnimation.setDuration(1500);
-		fadeInAnimation.setFillAfter(true);
-		
-		mAnswerLabel.setAnimation(fadeInAnimation);
-	}
-	
-	private void playSound() {
-		MediaPlayer player = MediaPlayer.create(this, R.raw.crystal_ball_sound);
-		player.start();
-		player.setOnCompletionListener(new OnCompletionListener() {
-			
-			@Override
-			public void onCompletion(MediaPlayer mp) {
-				mp.release();
-				
-			}
-		});
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 
-
-
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
-	
-	// Assigning the views from the layout file
-	mAnswerLabel = (TextView) findViewById(R.id.textView1);
-	mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
-	
-	
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
 }
-	
-	
